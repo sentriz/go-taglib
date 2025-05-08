dc3d4259e27bacb599d8fe0fff086f069f73a479
AaronS <fuzzyfonzy@yahoo.com>
2025-05-07 20:17:33 -0700

    adjusting build script, readme. removing generate from taglib.go.

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
-set -e
-# Clean up any previous build
-rm -rf build
-
-# Configure the build
 cmake -DWASI_SDK_PREFIX=/opt/wasi-sdk -DCMAKE_TOOLCHAIN_FILE=/opt/wasi-sdk/share/cmake/wasi-sdk.cmake -B build .
-
-# Build the project
-cd build
-make
-
-# Find the wasm file (it might be in a different location)
-WASM_FILE=$(find . -name "*.wasm")
-if [ -z "$WASM_FILE" ]; then
-  echo "No .wasm file found in the build directory"
-  exit 1
-fi
-
-# Move and optimize the wasm file
-cp "$WASM_FILE" ../taglib.wasm
-cd ..
+cmake --build build --target taglib
+mv build/taglib.wasm .
 wasm-opt --strip -c -O3 taglib.wasm -o taglib.wasm
-EOF
